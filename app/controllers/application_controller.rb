d578992621f4ba1586380f874a4ff480485f8d09
eugeniu velisca <veliscaeugeniu@gmail.com>
2023-12-14 14:54:25 +0100

    proposition de choix du pays en signed_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:country])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:country])
+  end
+
+  def after_sign_in_path_for(_user)
+    choices_path
+  end
+
+  def after_sign_up_path_for(_user)
+    choices_path
+  end
 end
