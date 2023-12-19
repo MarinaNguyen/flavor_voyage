528f1d1c490c3e23205b40db4775900d2ed1245b
eugeniu velisca <veliscaeugeniu@gmail.com>
2023-12-19 15:14:49 +0100

    stimulus dashboard

--- app/javascript/controllers/form_dashboard_controller.js
+++ app/javascript/controllers/form_dashboard_controller.js
@@ -2,13 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 
 export default class extends Controller {
-  static targets = ["form"];
+  static targets = ["form", "container2", "icon2"];
 
   connect() {
     this.formTarget.classList.add("d-none");
   }
 
-  displayForm() {
+  displayForm(event) {
+    if (event.target) {
+      this.icon2Target.classList.toggle("down")
+    }
     this.formTarget.classList.toggle("d-none");
   }
 }
